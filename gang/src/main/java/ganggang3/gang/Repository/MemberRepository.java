dd83697cf877276761253acd4a1ef095ab2baace
jurlring <78091011+jurlring@users.noreply.github.com>
2021-09-18 00:00:04 +0900

    MemberRepository수정

--- gang/src/main/java/ganggang3/gang/Repository/MemberRepository.java
+++ gang/src/main/java/ganggang3/gang/Repository/MemberRepository.java
@@ -2,8 +2,9 @@ package ganggang3.gang.Repository;
 
 import ganggang3.gang.domain.Member;
 import ganggang3.gang.domain.Myplace;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Member findById(long id);
 }

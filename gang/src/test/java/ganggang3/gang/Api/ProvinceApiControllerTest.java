6574822df32c27d7947ddc12bc13c748eba27b7e
Moon Yewan <mool2480@gmail.com>
2021-09-19 13:39:14 +0900

    change  juit5 to junit4 in myplaceApuTest

--- gang/src/test/java/ganggang3/gang/Api/ProvinceApiControllerTest.java
+++ gang/src/test/java/ganggang3/gang/Api/ProvinceApiControllerTest.java
@@ -1,31 +1,25 @@
 package ganggang3.gang.Api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@Transactional
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProvinceAPiController {
-
+public class ProvinceApiControllerTest {
     @Autowired
     MockMvc mockMvc;
 
@@ -40,4 +34,5 @@ public class ProvinceAPiController {
                 .andExpect(jsonPath("$.data[0].name").value("경기도"))
                 .andDo(print());
     }
+
 }

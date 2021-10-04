0b8a637acdf3fae97020b96e9f9d90aaba54bc19
skynnn <gksmf001105@naver.com>
2021-10-04 17:06:36 +0900

    PlaceDtoVlog 수정

--- gang/src/main/java/ganggang3/gang/dto/PlaceDtoVlog.java
+++ gang/src/main/java/ganggang3/gang/dto/PlaceDtoVlog.java
@@ -1,10 +1,9 @@
 package ganggang3.gang.dto;
 
+import ganggang3.gang.domain.Category;
 import ganggang3.gang.domain.Place;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -12,12 +11,14 @@ public class PlaceDtoVlog {
     private String name;
     private double location_x;
     private double location_y;
+    private Category category;
 
     public static PlaceDtoVlog of (Place P){
         return new PlaceDtoVlog(
                 P.getName(),
                 P.getLocation_x(),
-                P.getLocation_y()
+                P.getLocation_y(),
+                P.getCategory()
         );
     }
 }

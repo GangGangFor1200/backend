c9d3530ca59fe473255ed63702362d873b504b88
jurlring <78091011+jurlring@users.noreply.github.com>
2021-10-07 20:06:42 +0900

    MemberService 수정

--- gang/src/main/java/ganggang3/gang/Service/MemberService.java
+++ gang/src/main/java/ganggang3/gang/Service/MemberService.java
@@ -4,14 +4,6 @@ import ganggang3.gang.AuthorizationKakao;
 import ganggang3.gang.Repository.MemberRepository;
 import ganggang3.gang.domain.Member;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
